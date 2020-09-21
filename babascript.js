7adeef818246aae0d9be09f07bd8f89e3abcb7b4
Buck4437 <Buck4437@users.noreply.github.com>
2020-09-21 15:50:41 +0800

    Init

---
+++ babascript.js
@@ -0,0 +1,147 @@
+function lexer (code) {
+  return code.split(/\s+/)
+          .filter(t => t.length > 0)
+          .map(function (t) {
+                return !isNaN(t) ? {type: 'Number', value: t}
+                      : t === t.toUpperCase() ? {type: 'Property', value: t}
+                      : t.charAt(0) === t.charAt(0).toUpperCase() ? {type: 'Object', value: t}
+                      : {type: 'Operator', value: t}
+              })
+}
+
+function parser (tokens) {
+  var AST = {
+    type: 'BABASCRIPT',
+    body: []
+  }
+  // extract a token at a time as current_token. Loop until we are out of tokens.
+  while (tokens.length > 0){
+    var current_token = tokens.shift()
+
+    if (current_token.type === 'Object') {
+      var operator_token = tokens.shift()
+      if (operator_token.type === 'Operator') {
+        let argument
+        switch (operator_token.value){
+          case 'is' :
+            argument = tokens.shift()
+            if (argument.type === 'Property' || argument.type === 'Object' || argument.type === 'Number') {
+              var expression = {
+                type: 'Operator',
+                name: 'is',
+                arguments: []
+              }
+              expression.arguments.push({  // add argument information to expression object
+                type: 'Object',
+                value: current_token.value
+              })
+              expression.arguments.push({  // add argument information to expression object
+                type: argument.type,
+                value: argument.value
+              })
+              AST.body.push(expression)    // push the expression object to body of our AST
+            } else {
+              throw '"is" must be followed by a noun.'
+            }
+          break;
+          case 'log' :
+            argument = tokens.shift()
+            if (argument.type === 'Object' || argument.type === 'Number') {
+              var expression = {
+                type: 'Command',
+                name: 'log',
+                arguments: []
+              }
+              expression.arguments.push({  // add argument information to expression object
+                type: 'Object',
+                value: current_token.value
+              })
+              expression.arguments.push({  // add argument information to expression object
+                type: argument.type,
+                value: argument.value
+              })
+              AST.body.push(expression)    // push the expression object to body of our AST
+            } else {
+              throw '"log" must be followed by a noun.'
+            }
+          break;
+        }
+      }
+      else{
+        throw ('An Object must be followed by an Operator')
+      }
+    }
+  }
+  return AST
+}
+
+function transpiler(ast){
+  var vars = {}
+  while (ast.body.length > 0) {
+    var node = ast.body.shift()
+    var args = node.arguments
+    switch (node.name) {
+      case 'is' :
+        if (args[1].type === 'Property'){
+          switch (args[1].value){
+            case 'Num':
+            case 'Number':
+            vars[args[0].value] = 0
+
+            case 'Str':
+            case 'String':
+            vars[args[0].value] = ""
+          }
+        }
+        else if (args[1].type === 'Object'){
+          if (vars[args[1].value] !== undefined){
+              vars[args[0].value] = vars[args[1].value]
+          }
+          else{
+            throw (`Error: ${args[1].value} is not defined`)
+          }
+        }
+        else if (args[1].type === 'Number'){
+          vars[args[0].value] = Number(args[1].value)
+        }
+        else {
+          vars[args[0].value] = args[1].value
+        }
+      break
+      case 'log' :
+        if (args[1].type === 'Object'){
+          if (vars[args[1].value] !== undefined){
+              console.log(`${args[0].value} log: ${vars[args[1].value]}`)
+          }
+          else{
+            throw (`Error: ${args[1].value} is not defined`)
+          }
+        }
+        else {
+          console.log(`${args[0].value} log: ${args[1].value}`)
+        }
+      break
+    }
+  }
+}
+
+function run(str){
+  let tokens = lexer(str)
+  let ast = parser(tokens)
+  transpiler(ast)
+}
+
+
+// window.Baba = (function(){
+//   'use strict'
+//
+//   var Baba = {}
+//
+//   Baba.run = function(str){
+//     let tokens = lexer(str)
+//     let tree = parser(tokens)
+//     return tree
+//   }
+//
+//   return Baba;
+// })()
